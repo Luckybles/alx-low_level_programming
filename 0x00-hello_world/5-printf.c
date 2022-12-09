db54a79573db2a1cbce47f60071398434a65f515
luckybles <blesssmith169@gmail.com>
2022-12-08 17:05:56 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*
+ * main - a program that prints with proper grammer, but the outcome is a piece of art,
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,");
+	return (0);
