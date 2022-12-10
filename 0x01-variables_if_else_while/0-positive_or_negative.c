ae78b794302f7cf2c7100de3424a4fd12e7b4d28
luckybles <blesssmith169@gmail.com>
2022-12-09 21:34:25 -0800

    positvive negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/** main - print if the n is positive,negativer or 0
+/* main - print if the n is positive,negativer or 0
  *
  * Return: 0
  */
