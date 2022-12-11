45b90ac4b358cfd30a8ea3e1e0d9ba1192916ad6
luckybles <blesssmith169@gmail.com>
2022-12-11 11:10:46 -0800

    print_tebaphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry block
+ * decription:prints the lowercase alphabet in reverse
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char s = 'z';
+
+	while (s >= 'a')
+		putchar(s);
+		s--;
+		putchar('\n');
+
+		return (0);
+}
