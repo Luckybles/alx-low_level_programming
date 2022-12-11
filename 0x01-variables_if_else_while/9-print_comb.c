eb83ca251470cef03c53f30aa2da5ab0d034ae0a
luckybles <blesssmith169@gmail.com>
2022-12-11 13:19:59 -0800

    print combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry block
+ * description:prints all possible combinations of single-digit numbers
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+		putchar(number ",");
+	return (0);
+}
