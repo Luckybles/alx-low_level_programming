4d658206aec7fb42f75e9c3f6c54cdf101dfff03
luckybles <blesssmith169@gmail.com>
2022-12-11 11:39:39 -0800

    print_base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,9 +8,12 @@ int main(void)
 {
 	char digit;
 
-	for (digit = 0; digit <= 9; digit++)
-	for (digit = 'a'; digit <= 'f'; digit++)
-		putchar(digit);
+	while (digit <= 9)
+	{
+		for (digit = 'a'; digit <= 'f', digit++)
+			putchar(digit);
+		digit++;
+	}
 	putchar('\n');
 	return (0);
 }
