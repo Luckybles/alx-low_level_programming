ddb5e028bd1599e9b6f00241d137b71cb900691b
luckybles <blesssmith169@gmail.com>
2022-12-12 15:20:46 -0800

    combinatiom

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,7 +11,8 @@ int main(void)
 	for (number = 0; number <= 9; number++)
 	{
 		putchar(number + '0');
-		if (number != -1)
+
+		if (number = 0 || number < 10)
 		{
 				putchar(',');
 				putchar(' ');
