79d3b96bc659253537b806cd2f25f623b4605e25
luckybles <blesssmith169@gmail.com>
2022-12-13 04:56:38 -0800

    islower than

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _islower -check for lowercase
+ * Return: 1 if c is lower or return 0 if c is greater
+ */
+int _islower(int c)
+{
+	int r;
+
+	for (r = 'A'; r <= 'Z'; r++)
+	{
+		if (r != 'C')
+		{
+			_putchar(0 +'0');
+		}
+		else
+		{
+			_putchar(1 +'0');
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
