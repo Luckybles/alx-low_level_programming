2bcf5d39d00067f391ded47f8205e763e72bad8d
luckybles <blesssmith169@gmail.com>
2022-12-13 15:56:35 -0800

    jack baure

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * jack_bauer - print 24 hours
+ * Return: 00:00 
+ * 23:59
+ */
+void jack_bauer(void)
+{
+	int hour;
+	int minute;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60 ; minute++)
+		{
+			_putchar('0');
+			_putchar(minute + '0');
+		}
+		_putchar('\n');
+		_putchar(':');
+	}
