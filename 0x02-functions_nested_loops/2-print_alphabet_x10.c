b2ff30187c0c88d7a835eec32677a29bf0473d45
luckybles <blesssmith169@gmail.com>
2022-12-13 03:42:37 -0800

    print alphabets

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,14 +10,14 @@ void print_alphabet_x10(void)
 	char alphabet = 'a';
 	int num = 1;
 
-	while (alphabet <= 'z')
+	while (num <= 10)
 	{
-		while (num <= 10)
+		while (alphabet <= 'z')
 		{
 			_putchar(alphabet);
-			num++;
+			alphabet++;
 		}
-		alphabet++;
+		num++;
 		_putchar('\n');
 	}
 }
