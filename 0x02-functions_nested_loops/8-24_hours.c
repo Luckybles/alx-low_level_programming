6cefe9f24d0b41b9b3ed644fdde8c8c466d092f6
luckybles <blesssmith169@gmail.com>
2022-12-13 16:25:14 -0800

    jack baure

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,10 +13,14 @@ void jack_bauer(void)
 	{
 		for (minute = 0; minute < 60 ; minute++)
 		{
-			_putchar('0');
-			_putchar(minute + '0');
+                        _putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+
+			_putchar((minute % 10) + '0');
+
+			_putchar('\n');
 		}
-		_putchar('\n');
-		_putchar(':');
 	}
 }
