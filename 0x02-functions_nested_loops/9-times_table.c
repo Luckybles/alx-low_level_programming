424935ae1127a781865f9321de07372a5136f6cd
luckybles <blesssmith169@gmail.com>
2022-12-14 04:30:51 -0800

    timetable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,7 +22,7 @@ void times_table(void)
 				_putchar(' ');
 			else
 				_putchar((product / 10) + '0');
-			_putchar((product % 10 ) + '0');
+			_putchar((product % 10) + '0');
 		}
 
 		_putchar('\n');
