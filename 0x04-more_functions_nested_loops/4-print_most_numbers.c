91251a508d4538485d386915de9eb873bd3d35f2
luckybles <blesssmith169@gmail.com>
2022-12-15 12:30:00 -0800

    print number except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,12 +8,12 @@ void print_most_numbers(void)
 {
 	int numb;
 
-	for (numb = '0'; numb <= '9'; numb++)
+	for (numb = 48; numb <= 57; numb++)
 	{
-		if (numb != 2 && numb != 4)
+		if (numb != 50 && numb != 52)
 		{
 			_putchar(numb);
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
