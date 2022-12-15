de1d64d3bd9972d57bc408e6766738496ab46196
luckybles <blesssmith169@gmail.com>
2022-12-15 12:03:02 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - print from 0 to 9
+ *
+ * Return: number from 0 to 9
+ */
+void print_numbers(void)
+{
+	int numb;
+
+	for (numb = 0; numb <= 9; numb++)
+	{
+		_putchar(numb);
+		_putchar('\n');
+	}
+}
