33c4492eafc56af50cece0d29f46dac72578dcd4
luckybles <blesssmith169@gmail.com>
2022-12-16 01:38:57 -0800

    print in suare

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_square - print a square
+ * @size:  size of square
+ * Return: void
+ */
+void print_square(int size)
+{
+	int firstloop;
+	int secondloop;
+
+	if (size < = 0)
+		_putchar('\n');
+
+	for (firstloop = 0; firstloop < size; firstloop++)
+	{
+		for (secondloop = 0; secondloop < size; secondloop++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
