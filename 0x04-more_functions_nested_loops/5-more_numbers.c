e09590f9c8770d44e06efa75da679b5a34583f91
luckybles <blesssmith169@gmail.com>
2022-12-15 16:25:19 -0800

    print nested loop

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,11 +14,9 @@ void more_numbers(void)
 		for (mult = 0; mult <= 14; mult++)
 		{
 			if (mult >= 10)
-			{
 				_putchar('1');
-				_putchar(mult % 10 + '0');
-			}
+			_putchar(mult % 10 + '0');
 		}
-			_putchar('\n');
+		_putchar('\n');
 	}
 }
