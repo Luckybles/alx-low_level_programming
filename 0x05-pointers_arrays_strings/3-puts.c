44d1c300b57377f4ecfbe4e77c18377b7fe7fa2b
luckybles <blesssmith169@gmail.com>
2022-12-19 10:55:45 -0800

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _puts - print a string
- * @s: string
- * Return: a string
+ * _puts - print a string followed by a new line
+ * @str: string to print
+ *
  */
 void _puts(char *str)
 {
