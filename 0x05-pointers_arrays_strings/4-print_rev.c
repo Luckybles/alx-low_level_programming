da2aab45df996048615925965c7eb61331beca0d
luckybles <blesssmith169@gmail.com>
2022-12-19 11:03:54 -0800

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - print a string in reverse followed by a newline
+ * @s: string
+ */
+void print_rev(char *s)
+{
+	int length = 0;
+	int rev;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	for (rev = length; rev > 0; rev--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
