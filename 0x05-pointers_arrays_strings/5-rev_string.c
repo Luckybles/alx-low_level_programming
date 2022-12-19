731f83fd03cdadda0f8754f913976ca83970c1f9
luckybles <blesssmith169@gmail.com>
2022-12-19 12:52:33 -0800

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: input string
+ * Return: string in reverse
+ */
+void rev_string(char *s)
+{
+	int collect = 0;
+	int i;
+	char reverse =s[0];
+
+	while( *s != '\0')
+		collect++;
+	for (i = 0; i< collect; i++)
+	{
+		reverse = s[i];
+		s[i] = s[collect];
+		s[collect] = reverse;
+	}
+}
