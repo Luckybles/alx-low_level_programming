974c18e4e484adf091cc8f6ec29f0e8c22c8a342
luckybles <blesssmith169@gmail.com>
2022-12-19 06:36:54 -0800

    strlength

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen - return length of string
+ * @s: sting
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	for (length = 0; length != '\0'; length++)
+		return (length);
+}
