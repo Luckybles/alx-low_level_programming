3247614051c7b6b35091d5658612b68a8e9d6947
luckybles <blesssmith169@gmail.com>
2022-12-20 10:30:00 -0800

    array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_array - prints elements of an array
+ * @a: array input
+ * @n: n element
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", *(a + 1));
+
+		if (i != (n - 1))
+			printf(" ");
+	}
+	printf('\n');
+}
