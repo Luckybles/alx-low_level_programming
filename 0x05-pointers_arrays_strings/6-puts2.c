be1fdc15f4eb7db0aa82acc26929a0222db61956
luckybles <blesssmith169@gmail.com>
2022-12-19 22:38:41 -0800

    puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2 - print every character of a string
+ * @str: string
+ * Return: every character of a string
+ */
+void puts2(char *str)
+{
+	int collect;
+	int i;
+
+	while (str != '\0')
+		collect++;
+	for (i = 0; i < collect; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(collect[i];
+	}
+	_putchar('\n');
+}
