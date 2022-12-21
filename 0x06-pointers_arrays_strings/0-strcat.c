3b5e8f43afee37b1d4b7f205a6e626c88aa16634
luckybles <blesssmith169@gmail.com>
2022-12-21 15:35:05 -0800

    concantinate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,13 +8,15 @@
 char *_strcat(char *dest, char *src)
 {
 	while (*dest != '\0')
-		dest++;
-	while (*src != '\0')
 	{
-		*dest = *src;
 		dest++;
-		src++;
+		while (*src != '\0')
+		{
+			*dest = *src;
+			dest++;
+			src++;
+		}
+		*dest = '\0';
+		return (dest);
 	}
-	*dest = '\0';
-	return dest;
 }
