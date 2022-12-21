2ab3b82c9ed0bbbb0c96e073231b96d2221a0ccf
luckybles <blesssmith169@gmail.com>
2022-12-21 15:13:40 -0800

    concantinate

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcat - concatenate two strings
+ * @dest: input string
+ * @src: input string
+ * Return: pointer to the string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	while (*dest != '\0')
+		dest++;
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return dest;
+}
