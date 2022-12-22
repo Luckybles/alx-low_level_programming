5ba906b3098d23422e14798dfcbe7e66a0f96448
luckybles <blesssmith169@gmail.com>
2022-12-22 09:07:49 -0800

    string to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * string_toupper - function to change all lowercase to uppercase
+ * @n: pointer
+ * Return: char
+ */
+char *string_toupper(char *n)
+{
+	int j;
+
+	j = 0;
+	while (n[j] != '\0')
+	{
+		if (n[j] >= 'a'; && n[j] <= 'z')
+			n[j] = n[j] - 32;
+	}
+}
