df452f9e62b1786772faedec8eb57b8cc03f9da0
luckybles <blesssmith169@gmail.com>
2022-12-22 12:52:24 -0800

    captiletter

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,7 +16,8 @@ char *cap_string(char *str)
 		    str[i - 1] == '\t' ||
 		    str[i - 1] == '\n' ||
 		    str[i - 1] == ',' ||
-		    str[i - 1] == '_' ||
+		    str[i - 1] == ';' ||
+		    str[i - 1] == '.' ||
 		    str[i - 1] == '!' ||
 		    str[i - 1] == '?' ||
 		    str[i - 1] == '"' ||
