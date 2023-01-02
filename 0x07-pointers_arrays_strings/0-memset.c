ae71ee0c8e6566ec20d27ca3925bc6d5bf5cfb2c
luckybles <blesssmith169@gmail.com>
2023-01-02 10:43:04 -0800

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,7 +2,7 @@
 /**
  * _memset - fills memory with a constant byte
  * @s: source string
- * @b the constant byte for filling
+ * @b: the constant byte for filling
  * @n : length of buffer
  * Return: new string
  */
