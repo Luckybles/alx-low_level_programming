0551acae990c6b47c6dd8432694c9e204b5f96d1
luckybles <blesssmith169@gmail.com>
2023-01-03 11:22:01 -0800

    string character

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,14 +9,15 @@ char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
 	{
-		if (*s == 'c')
+		if (*s == c)
 		{
 			return (s);
 		}
-		if (*s != 'c')
-		{
-			return (NULL);
-		}
+		++s;
+	}
+	if (*s == c)
+	{
+		return (s);
 	}
 	return (0);
 }
