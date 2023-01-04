83b8da8a4def274ae610412c91a1e3f113fa8bb4
luckybles <blesssmith169@gmail.com>
2023-01-04 03:55:13 -0800

    stringline

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,14 +6,13 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
-	{
-		return;
-	}
-	else
+	if (*s == '\0')
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-	_putchar('\n');
+	else
+	{
+		_putchar('\n');
+	}
 }
