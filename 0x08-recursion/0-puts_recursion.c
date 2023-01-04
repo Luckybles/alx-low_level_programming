8c40afa4d6c390b0fbc8ea97bd5690ba24ac97f5
luckybles <blesssmith169@gmail.com>
2023-01-04 03:45:49 -0800

    stringline

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts_recursion - print a string follwed by a newline
+ * @s: string variable
+ * Return: a pointer to the function
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return;
+	}
+	else 
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	_putchar('\n');
+}
