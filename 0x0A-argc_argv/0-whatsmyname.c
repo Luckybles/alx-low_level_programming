e8bb465972e0a0e2a9f16a5e20b7cd0304c212b7
luckybles <blesssmith169@gmail.com>
2023-01-09 05:53:58 -0800

    whatmynme

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,7 +3,7 @@
 
 /**
  * main - print its name followed by a new line
- * @agrc: the number to count by command line
+ * @argc: the number to count by command line
  * @argv: arry that contain the program line to be counted
  * Return: 0
  */
