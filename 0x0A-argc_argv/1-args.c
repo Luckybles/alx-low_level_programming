923bb82245f2bedf4c6e8aa29e5fbaa4eaee41bd
luckybles <blesssmith169@gmail.com>
2023-01-09 07:54:44 -0800

    write the number of count

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include "main.h"
-/** main - print the number of argument passed 
- * @argc: the variable that counts the argument 
+/**
+ * main - print the number of argument passed
+ * @argc: the variable that counts the argument
  * @argv: the varible that display the content
  * Return:0 (success)
  */
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d\n",argc - 1);
+	printf("%d\n", argc - 1);
 	return (0);
 }
