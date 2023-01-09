a51cbb38a233c56e1fdaeec5e92f7b21523eca56
luckybles <blesssmith169@gmail.com>
2023-01-09 08:04:31 -0800

    numbers of arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print all the argument it receives
+ * @argc: the argument counter
+ * @argv: the argument that is been counted
+ * Return: 0(success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", arg[i]);
+	}
+	return (0);
+}
