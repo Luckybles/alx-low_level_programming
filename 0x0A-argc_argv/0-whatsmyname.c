14658996b03599cd7d57ef226c6820b4c8396155
luckybles <blesssmith169@gmail.com>
2023-01-09 05:50:41 -0800

    whatmynme

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print its name followed by a new line
+ * @agrc: the number to count by command line
+ * @argv: arry that contain the program line to be counted
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s", *argv);
+	return (0);
+}
