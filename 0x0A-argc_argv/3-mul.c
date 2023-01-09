d74b5e244fb36d236da25a56de08ed42e8e1fa09
luckybles <blesssmith169@gmail.com>
2023-01-09 09:03:01 -0800

    multiply

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -15,11 +15,12 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-
-	
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-	result = num1 * num2;
-	printf("%d\n", result);
+	else
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		result = num1 * num2;
+		printf("%d\n", result);
+	}
 	return (0);
 }
