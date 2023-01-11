c6b2ce2c34fa5af0de39577796407af9310fd64f
luckybles <blesssmith169@gmail.com>
2023-01-10 20:45:23 -0800

    is not easy here in alx

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - create an array of char
+ * @c: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i;
+
+
+	arr = (char *)malloc(size * sizeof(c));
+
+	if (arr == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		i = 0;
+
+		while (i < size)
+		{
+			*(arr + i) = c;
+			i++;
+		}
+		return (arr);
+	}
+}
