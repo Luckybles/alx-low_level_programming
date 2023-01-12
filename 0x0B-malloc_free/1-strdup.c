2d71180f6d6d0c61cf3eb13a763a4aee25af1297
luckybles <blesssmith169@gmail.com>
2023-01-12 09:43:05 -0800

    strlen

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,26 +1,63 @@
 #include <stdio.h>
 #include "main.h"
 /**
+ * _strlen - count array
+ * @s: array of elemtents
+ * Return: 1
+ */
+int _strlen(char *s)
+{
+	unsigned int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * _strcpy - copy array
+ * @src: array of element
+ * @dest: dest array
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+/**
  * _strdup - return a pointer to newly allocated space in memory
  * @str: string to duplicate
  * Return: pointer to the copied string
  */
+
 char *_strdup(char *str)
 {
-	char *duplicate;
-	unsigned int i, len;
+	char *dst;
+	unsigned int size;
 
-	i = 0;
-	len = 0;
+	if (str == 0)
+	{
+		return (NULL);
+	}
+	size = _strlen(str) + 1;
+	dst = (char *) malloc(size * sizeof(char));
 
-	if (str == NULL)
+	if (dst == 0)
+	{
 		return (NULL);
-	while (str[len])
-		len++;
-	duplicate = malloc(sizeof(char) * (len + 1));
-	if (duplicate == NULL)
-		return (NULL);
-	while ((duplicate[i] = str[i]) != '\0')
-		i++;
-	return (duplicate);
+	}
+	_strcpy(dst, str);
+	return (dst);
 }
