4c48306e3c712fce6e3cf61c5ab29938e4419c9d
luckybles <blesssmith169@gmail.com>
2023-02-02 15:13:44 -0800

    binary unit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,23 +9,16 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int j;
-	int length, base_two;
+	unsigned int dec_val = 0;
+	int j;
 
 	if (!b)
 		return (0);
-	j = 0;
-	for (length = 0; b[len] != '\0'; length++)
-		for (length--; base_two = 1; length >= 0; length--, base_two *= 2)
-		{
-			if (b[length] != '0' && b[length] != '1')
-			{
-				return (0);
-			}
-			if (b[length] & 1)
-			{
-				j += base_two;
-			}
-		}
-	return (j);
+	for (j = 0; b[j]; j++)
+	{
+		if (b[j] < '0' || b[j] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[j] - '0');
+	}
+	return (dec_val);
 }
